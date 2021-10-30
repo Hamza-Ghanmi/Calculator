d4b3fe8da24b5fc5c961860175acd79b349c301c
Hamza-Ghanmi <ghanmi7amza@gmail.com>
2021-10-30 18:37:42 +0200

    BMI Calculator added

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -14,6 +14,18 @@ app.post("/", (req, res) => {
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
     res.send("The result of the calculation is "+ result);
+})
+
+app.get("/bmiCalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmiCalculator", (req, res) => {
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight / (height*height);
+    res.send("Your BMI is "+ bmi);
 })
 
 app.listen(3000, () => {
